64004b04c88e625f36f5363ae42f17df1ec27baf
George Bockari <bitwhys@gmail.com>
2023-06-16 05:20:03 -0400

    fix(app/page): escapte special characters

--- app/page.tsx
+++ app/page.tsx
@@ -14,10 +14,10 @@ export default function IndexPage() {
         <div className="text-center">
           <Badge variant="outline">Public Alpha</Badge>
           <h1 className="mt-4 text-3xl font-bold tracking-tight sm:text-5xl">
-            There's a new dawn on the horizon
+            There&apos;s a new dawn on the horizon
           </h1>
           <p className="mt-6 text-base leading-7 text-muted-foreground">
-            We're building something special, playground with out builder
+            We&apos;re building something special, playground with out builder
           </p>
           <div className="mt-10 flex items-center justify-center gap-x-6">
             <Link href="/builder" className={cn(buttonVariants({}))}>
